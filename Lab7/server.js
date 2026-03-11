39c60e3ba6945c4119df4b78be43e84fb08198c5
Yashkinople <yash@kinople.com>
2026-03-11 12:15:33 +0530

    lab7 task 1 completed

--- Lab7/server.js
+++ Lab7/server.js
@@ -1,30 +1,67 @@
+// const express = require("express");
+// const client = require("prom-client");
+
+// const app = express();
+// const port = 3000;
+
+// // GPS location of classroom (example)
+// const gpsData = {
+//     latitude: 12.9716,
+//     longitude: 77.5946,
+//     classroom: "CS Lab"
+// };
+
+// // Prometheus metrics
+// const register = new client.Registry();
+// client.collectDefaultMetrics({ register });
+
+// app.get("/gps", (req, res) => {
+//     res.json(gpsData);
+// });
+
+// // Prometheus metrics endpoint
+// app.get("/metrics", async (req, res) => {
+//     res.set("Content-Type", register.contentType);
+//     res.end(await register.metrics());
+// });
+
+// app.listen(port, () => {
+//     console.log(`GPS Microservice running on port ${port}`);
+// });
 const express = require("express");
-const client = require("prom-client");
 
 const app = express();
 const port = 3000;
 
-// GPS location of classroom (example)
-const gpsData = {
-    latitude: 12.9716,
-    longitude: 77.5946,
-    classroom: "CS Lab"
+let classroomLocation = {
+    latitude: null,
+    longitude: null
 };
 
-// Prometheus metrics
-const register = new client.Registry();
-client.collectDefaultMetrics({ register });
+// API to update location
+app.get("/update-location", (req,res)=>{
 
-app.get("/gps", (req, res) => {
-    res.json(gpsData);
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+
+    classroomLocation.latitude = lat;
+    classroomLocation.longitude = lon;
+
+    res.json({
+        message: "Location updated",
+        latitude: lat,
+        longitude: lon
+    });
+
 });
 
-// Prometheus metrics endpoint
-app.get("/metrics", async (req, res) => {
-    res.set("Content-Type", register.contentType);
-    res.end(await register.metrics());
+// API to get current location
+app.get("/gps",(req,res)=>{
+
+    res.json(classroomLocation);
+
 });
 
-app.listen(port, () => {
-    console.log(`GPS Microservice running on port ${port}`);
+app.listen(port,"0.0.0.0",()=>{
+    console.log("GPS Microservice running on port 3000");
 });
